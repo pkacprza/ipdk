95575eb38ccc0cc7d5c7b6ccf091c8890b20d754
rilnickx <rafalx.ilnicki@intel.com>
2022-08-12 14:40:56 +0200

    Rework setup.sh
    Signed-off-by: rilnickx <rafalx.ilnicki@intel.com>

--- build/storage/ptf_test/setup.sh
+++ build/storage/ptf_test/setup.sh
@@ -4,11 +4,17 @@ branch='feat-add-ptf-tests'
 git_url='https://github.com/intelfisz/ipdk.git'
 ptf_url='https://github.com/p4lang/ptf.git'
 
-mkdir -p "IPDK_workspace"
 current_dir=$(pwd)
-path_to_clone_ptf="$current_dir/IPDK_workspace/ipdk/build/storage/"
+storage_path="$current_dir/IPDK_workspace/ipdk/build/storage/"
+spdk_path="$storage_path/spdk"
 
+mkdir -p "IPDK_workspace"
 cd "IPDK_workspace" && git clone --branch "$branch" "$git_url"
-cd "$path_to_clone_ptf" && git clone "$ptf_url"
-
+cd "$storage_path" && git clone "$ptf_url"
+cd "$storage_path" && git submodule update --init --recursive --force
+cd "$spdk_path"
+sudo scripts/pkgdep.sh
+sudo dnf install -y kernel-headers
+sudo ./configure --with-sma --without-isal
+sudo make
 cd && mkdir -p "IPDK_workspace/SHARE"
