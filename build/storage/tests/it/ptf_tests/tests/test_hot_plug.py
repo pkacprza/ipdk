ead62bb7d4bb4fab2956807ca08535cc16754cbc
rilnickx <rafalx.ilnicki@intel.com>
2022-10-11 11:51:33 +0200

    [tests] Add send_command_over_unix_socket
    Signed-off-by: rilnickx <rafalx.ilnicki@intel.com>

--- build/storage/tests/it/ptf_tests/tests/test_hot_plug.py
+++ build/storage/tests/it/ptf_tests/tests/test_hot_plug.py
@@ -1,13 +1,31 @@
 import re
+import socket
+import logging
 from ptf.base_tests import BaseTest
 from test_connection import BaseTerminalMixin
 from system_tools.const import NQN, NVME_PORT, SPDK_PORT
 from system_tools.ssh_terminal import CommandException
-from scripts.socket_functions import send_command_over_unix_socket
+
+
+def send_command_over_unix_socket(sock: str, cmd: str, wait_for_secs: float) -> str:
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+        s.settimeout(wait_for_secs),
+        out = []
+        try:
+            s.connect(sock)
+            cmd = f"{cmd}\n".encode()
+            s.sendall(cmd)
+            while data := s.recv(256):
+                out.append(data)
+        except socket.timeout:
+            logging.error("Timeout exceeded")
+        return b"".join(out).decode()
+
 
 def get_docker_containers_id_from_docker_image_name(terminal, docker_image_name):
     out = terminal.execute(f'sudo docker ps | grep "{docker_image_name}"')
     return [line.split()[0] for line in out]
+
 
 class TestCreateAndExposeSubsystemOverTCP(BaseTerminalMixin, BaseTest):
     def setUp(self):
@@ -47,14 +65,14 @@ class TestCreateRamdriveAndAttachAsNsToSubsystem64(BaseTerminalMixin, BaseTest):
         self.terminal = self.ipu_storage_terminal
         t = 60
         pattern = 'vm.qcow2'
-        out, _ = self.terminal.execute(cmd=f"ls {SHARE_DIR_PATH}")
+        out = ''.join(self.terminal.execute(cmd=f"ls {SHARE_DIR_PATH}"))
         result = re.search(pattern=pattern, string=out)
         if result is None:
             t = 420
         cmd = f'sudo SHARED_VOLUME={SHARE_DIR_PATH} UNIX_SERIAL=vm_socket scripts/vm/run_vm.sh &> /dev/null &'
         self.terminal.execute(f"cd {STORAGE_PATH} && {cmd}")
         time.sleep(t)
-        out, _ = self.terminal.execute(cmd=f"ls {SHARE_DIR_PATH}")
+        out = ''.join(self.terminal.execute(cmd=f"ls {SHARE_DIR_PATH}"))
         result = re.search(pattern=pattern, string=out)
         assert result
 
