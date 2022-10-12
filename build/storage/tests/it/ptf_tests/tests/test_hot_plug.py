4e228fe29a6753ff7199743a72386dc80a140a04
rilnickx <rafalx.ilnicki@intel.com>
2022-10-12 12:12:08 +0200

    [tests] Debugging
    Signed-off-by: rilnickx <rafalx.ilnicki@intel.com>

--- build/storage/tests/it/ptf_tests/tests/test_hot_plug.py
+++ build/storage/tests/it/ptf_tests/tests/test_hot_plug.py
@@ -71,9 +71,9 @@ class TestCreateRamdriveAndAttachAsNsToSubsystem64(BaseTerminalMixin, BaseTest):
         self.terminal = self.storage_target_terminal
         vm = VirtualMachine(StorageTargetPlatform())
         vm.run()
-        out = send_command_over_unix_socket(self.vm.socket_path, "root", 1)
+        out = send_command_over_unix_socket(vm.socket_path, "root", 1)
         print(out)
-        out = send_command_over_unix_socket(self.vm.socket_path, "root", 1)
+        out = send_command_over_unix_socket(vm.socket_path, "root", 1)
         print(out)
         self.cmd_sender_id = get_docker_containers_id_from_docker_image_name(self.terminal, "cmd-sender")[0]
 
