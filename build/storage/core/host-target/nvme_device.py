f5974b7dd219f3447c24a446edec59347be7bd93
Artsiom Koltun <artsiom.koltun@intel.com>
2022-10-26 12:27:30 +0200

    Refactor host-target model to enable virtual functions.
    IPDK intends to support different devices and KVM case exists for
    demonstration purposes. Despite many pros it also has some
    disadvantages like luck of VFs support for NVMe and only PF
    enabling is demonstrated.
    
    In this patch the host-target model and code are reworked to provide
    a target-agnostic way for PFs and VFs to appear within host OS.
    For HW targets, it is required to provide their own SMA handle class
    which can supply needed information like finding a corresponding
    PCI address, info if provided SMA handle refers to a virtual device
    or used protocol.
    
    PF/VF functionality is covered with unit tests against fake sysfs
    with, at this moment, limited functionality.
    
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

---
+++ build/storage/core/host-target/nvme_device.py
@@ -0,0 +1,77 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+import logging
+from typing import Callable
+from storage_pcie_device import StoragePcieDevice
+from pci_devices import PciAddress
+from device_driver import DeviceDriver
+from sriov_device_driver import SriovDeviceDriver
+from device_exerciser_if import DeviceExerciserError
+
+
+class NvmeDevice(StoragePcieDevice):
+    def __init__(
+        self,
+        pci_addr: PciAddress,
+        driver: DeviceDriver,
+        volume_detector: Callable,
+        fio_runner: Callable,
+    ) -> None:
+        super().__init__(pci_addr, driver, volume_detector, fio_runner)
+
+
+class NvmePfDevice(NvmeDevice):
+    def __init__(
+        self,
+        pci_addr: PciAddress,
+        sriov_device_driver: SriovDeviceDriver,
+        volume_detector: Callable,
+        fio_runner: Callable,
+    ) -> None:
+        super().__init__(pci_addr, sriov_device_driver, volume_detector, fio_runner)
+        self._sriov_driver = sriov_device_driver
+
+    def plug(self) -> None:
+        super().plug()
+        logging.info(f"Pf {self._pci_addr} is bound to driver")
+        if self._sriov_driver.is_sriov_supported(
+            self._pci_addr
+        ) and not self._sriov_driver.is_sriov_enabled(self._pci_addr):
+            logging.info(f"Pf {self._pci_addr} enabling SRIOV")
+            self._sriov_driver.enable_sriov(self._pci_addr)
+            logging.info(f"Pf {self._pci_addr} SRIOV enabled")
+
+    def unplug(self) -> None:
+        if self._sriov_driver.are_vfs_enabled(self._pci_addr):
+            raise DeviceExerciserError(
+                f"Cannot delete pf {self._pci_addr} with bound vfs"
+            )
+
+        if self._sriov_driver.is_sriov_supported(
+            self._pci_addr
+        ) and self._sriov_driver.is_sriov_enabled(self._pci_addr):
+            logging.info(f"Pf {self._pci_addr} disabling SRIOV")
+            self._sriov_driver.disable_sriov(self._pci_addr)
+            logging.info(f"Pf {self._pci_addr} SRIOV disabled")
+        super().unplug()
+
+    def is_plugged(self) -> bool:
+        if not super().is_plugged():
+            return False
+        if self._sriov_driver.is_sriov_supported(
+            self._pci_addr
+        ) and not self._sriov_driver.is_sriov_enabled(self._pci_addr):
+            return False
+        return True
+
+
+class NvmeVfDevice(NvmeDevice):
+    def __init__(
+        self,
+        pci_addr: PciAddress,
+        driver: DeviceDriver,
+        volume_detector: Callable,
+        fio_runner: Callable,
+    ) -> None:
+        super().__init__(pci_addr, driver, volume_detector, fio_runner)
---
+++ build/storage/core/host-target/storage_pcie_device.py
@@ -0,0 +1,43 @@
+# Copyright (C) 2022 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+#
+import copy
+
+import copy
+
+from typing import Callable
+from pci_devices import PciAddress
+from device_driver import DeviceDriver
+from fio_args import FioArgs
+from volume import VolumeId
+
+
+class StoragePcieDevice:
+    def __init__(
+        self,
+        pci_addr: PciAddress,
+        driver: DeviceDriver,
+        volume_detector: Callable,
+        fio_runner: Callable,
+    ) -> None:
+        self._pci_addr = pci_addr
+        self._driver = driver
+        self._find_volumes = volume_detector
+        self._fio_runner = fio_runner
+
+    def run_fio_on_volumes(self, fio_args: FioArgs, volume_ids: set[VolumeId]) -> str:
+        volumes = self._find_volumes(self._pci_addr, volume_ids)
+        fio_args = copy.deepcopy(fio_args)
+        fio_args.add_volumes_to_exercise(volumes)
+        return self._fio_runner(fio_args)
+
+    def is_plugged(self) -> bool:
+        return self._driver.is_bound(self._pci_addr)
+
+    def plug(self) -> None:
+        if not self._driver.is_bound(self._pci_addr):
+            self._driver.bind(self._pci_addr)
+
+    def unplug(self) -> None:
+        if self._driver.is_bound(self._pci_addr):
+            self._driver.unbind(self._pci_addr)
